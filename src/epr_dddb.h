bd59610255df9c84ce8e0a39c76f795318342a15
Christian Hartl <christian.hartl@catalysts.cc>
2014-10-07 13:19:24 +0200

    Adding support for MERIS IODD8
    At least reading the SNOW_ICE MERIS L2 flag should work now.
    This should be checked by experts though.
    (I added TODOs in various places.)
    
    The main change was:
    - to rename MER_RR__2P_flags_data to MER_RR__2P_flags_IODD7_data
    - to derive a new version MER_RR__2P_flags_data from it:
       - Added SNOW_ICE
       - Replaced LARS_ON by DDV
       - Removed LOW_PRESSURE
       - Fixed typo in description of WHITE_SCATTERER (Presense -> Presence)
       - Fixed bug (?) in CASE2_S: Using bits 8+21 (was: 8 only, i.e. water bit 21 not set)
    
    The following references were used for this change:
    - http://www.brockmann-consult.de/beam-jira/browse/BEAM-1237?page=com.atlassian.jira.plugin.system.issuetabpanels:changehistory-tabpanel
    - https://github.com/bcdev/beam/blob/58f66c3d6697daab049abaa7284b26f652bfca5c/beam-envisat-reader/src/main/resources/org/esa/beam/resources/dddb/flags/MER_RR__2P_flags.dd
    - http://earth.eo.esa.int/pcs/envisat/meris/documentation/MERIS_IPF_evolution.pdf

--- src/epr_dddb.h
+++ src/epr_dddb.h
@@ -75,16 +75,16 @@ struct RecordDescriptorTable {
     const struct RecordDescriptor* descriptors;
 };
 
-extern const struct DatasetDescriptorTable dddb_product_tables[64];
-extern const struct BandDescriptorTable dddb_band_tables[37];
-extern const struct FlagDescriptorTable dddb_flag_coding_tables[6];
+extern const struct DatasetDescriptorTable dddb_product_tables[66];
+extern const struct BandDescriptorTable dddb_band_tables[39];
+extern const struct FlagDescriptorTable dddb_flag_coding_tables[7];
 extern const struct RecordDescriptorTable dddb_meris_rec_tables[23];
 extern const struct RecordDescriptorTable dddb_aatsr_rec_tables[20];
 extern const struct RecordDescriptorTable dddb_asar_rec_tables[22];
 
-#define EPR_NUM_PRODUCT_TABLES         64
-#define EPR_NUM_BAND_TABLES            37
-#define EPR_NUM_FLAG_CODING_TABLES     6
+#define EPR_NUM_PRODUCT_TABLES         66
+#define EPR_NUM_BAND_TABLES            39
+#define EPR_NUM_FLAG_CODING_TABLES     7
 #define EPR_NUM_MERIS_REC_TABLES       23
 #define EPR_NUM_AATSR_REC_TABLES       20
 #define EPR_NUM_ASAR_REC_TABLES        22
